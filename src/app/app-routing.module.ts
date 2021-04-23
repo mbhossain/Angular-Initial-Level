152f8030900b21e8570290ef8c30e1c77c9ac9b9
Mohammad Belal <billal@cokreates.com>
2021-04-24 04:43:46 +0600

    simple routing two component employee-list and department

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentComponent } from './department/department.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 const routes: Routes = [
-  { path: 'employee', component: EmployeeListComponent}
+  { path: 'employee', component: EmployeeListComponent},
+  { path: 'department', component: DepartmentComponent}
 ];
 
 @NgModule({
@@ -12,5 +14,6 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 export const routingComponents = [
-  EmployeeListComponent
+  EmployeeListComponent,
+  DepartmentComponent
 ]
