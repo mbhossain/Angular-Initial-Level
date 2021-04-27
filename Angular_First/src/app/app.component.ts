a11315760400e450231af133a6ad9176ca5a4511
Mohammad Belal <billal@cokreates.com>
2021-04-28 01:40:16 +0600

    pipe

--- Angular_First/src/app/app.component.ts
+++ Angular_First/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent {
 
   public name = "Mohammad Belal";
+  public message = "I want stop wast time forever my life...";
+  public person = {
+    name: "Marium Mahasanat Sara Atri",
+    age: 1,
+    district: "kushtia"
+  }
+
+  public date = new Date();
 
   constructor() {
   }
