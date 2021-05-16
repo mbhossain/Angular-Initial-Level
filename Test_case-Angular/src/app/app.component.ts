5705ebf2f330394f0513964159de986b9a0f7546
Mohammad Belal <king.cseiu@gmail.com>
2021-05-16 23:50:17 +0600

    ngFor

--- Test_case-Angular/src/app/app.component.ts
+++ Test_case-Angular/src/app/app.component.ts
@@ -8,4 +8,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'learn-angular ';
   age = 17;
+
+  // data = ["Motaleb", "Khadeja", "Belal", "Sadia", "Atri"];
+  data = [
+    {
+      name: 'Motaleb',
+      age: 56
+
+    },
+    {
+      name: 'Khadeja',
+      age: 50
+
+    },
+    {
+      name: 'Belal',
+      age: 32
+
+    },
+    {
+      name: 'Sadia',
+      age: 25
+
+    },
+    {
+      name: 'Atri',
+      age: 1
+
+    }
+  ];
 }
