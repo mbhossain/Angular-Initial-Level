9f6ce0b1ab9272b546a62e1ed46b2a7de791b5a9
Mohammad Belal <king.cseiu@gmail.com>
2021-06-10 18:22:26 +0600

    use reactive form

--- Test_case-Angular/src/app/modules/fetch-data-http/components/employee-list/employee-list.component.ts
+++ Test_case-Angular/src/app/modules/fetch-data-http/components/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service';
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-employee-list',
@@ -9,10 +10,20 @@ import { EmployeeService } from '../../services/employee.service';
 export class EmployeeListComponent implements OnInit {
   public employees = [];
 
-  constructor(private _employeeService: EmployeeService) { }
+  constructor(
+    private _employeeService: EmployeeService
+  ) { }
 
   ngOnInit() {
     this._employeeService.getEmployees().subscribe(data => this.employees = data);
+  }
+
+  timePicker = new FormGroup({
+    time: new FormControl(''),
+  })
+
+  getTime(){
+    console.log(this.timePicker.value);
   }
 
 }
--- Test_case-Angular/src/app/modules/fetch-data-http/fetch-data-http.module.ts
+++ Test_case-Angular/src/app/modules/fetch-data-http/fetch-data-http.module.ts
@@ -5,6 +5,7 @@ import { FetchDataHttpRoutingModule } from './fetch-data-http-routing.module';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
 import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [EmployeeListComponent, EmployeeDetailComponent],
@@ -12,6 +13,7 @@ import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
     CommonModule,
     FetchDataHttpRoutingModule,
     NgxMaterialTimepickerModule,
+    ReactiveFormsModule
   ]
 })
 export class FetchDataHttpModule { }
